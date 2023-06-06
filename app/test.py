a36d0d94d40e255ea19e143eb134a00660f29d09
謝佳穎 <chiayinghsieh212@gmail.com>
2023-06-06 22:00:08 +0800

    test remote lab computer connection

--- app/test.py
+++ app/test.py
@@ -17,30 +17,36 @@ def init_connections():
 
     connection1 = mysql.connector.connect(
         user=user, password=pwd, host='127.0.0.1', port="3306", database='vcdb')
-    globals.connection1 = connection1
-    globals.vc_cursor = connection1.cursor()
+    # globals.connection1 = connection1
+    # globals.vc_cursor = connection1.cursor()
     print("VCDB connected.")
 
     connection2 = mysql.connector.connect(
         user=user, password=pwd, host='127.0.0.1', port="3306", database='userdb')
-    globals.connection2 = connection2
-    globals.user_cursor = connection2.cursor()
+    # globals.connection2 = connection2
+    # globals.user_cursor = connection2.cursor()
     print("user DB connected.")
 
-    # insert data for testing
-    query = "insert into user values (%s, %s, %s, %s, %s);"
-    values = ["test2", "aMember", "member.com", "testtt2", "func2"]
-    globals.vc_cursor.execute(query, values)
-    globals.connection2.commit()
-    globals.userid = "test2"
+    connection3 = mysql.connector.connect(
+        user="root", password="mypassword", host='140.112.106.237', port="16918", database='userdb')
+    # globals.connection2 = connection2
+    # globals.user_cursor = connection2.cursor()
+    print("lab DB connected.")
 
-    print(globals.connection1, globals.vc_cursor, globals.user_cursor)
+    # insert data for testing
+    # query = "insert into user values (%s, %s, %s, %s, %s);"
+    # values = ["test2", "aMember", "member.com", "testtt2", "func2"]
+    # globals.vc_cursor.execute(query, values)
+    # globals.connection2.commit()
+    # globals.userid = "test2"
+
+    # print(globals.connection1, globals.vc_cursor, globals.user_cursor)
     return
 
 
 init_connections()
 
 # test functions
-checkout("func1", False)
-checkout("func3", False)
-checkout("func3", True)
+# checkout("func1", False)
+# checkout("func3", False)
+# checkout("func3", True)
